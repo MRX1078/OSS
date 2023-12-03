08aa0cc11edf8c1a40efdb1ceab1c11b5f4669f0
he0982 <Keithblack444@yandex.ru>
2023-12-03 09:36:17 +0300

    Relise 2.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-    Button,
-    Container,
-    Paper,
-    Tab,
-    Tabs,
-    TextField,
-    Typography,
-} from '@mui/material';
-import economy from '/home/he0982/WebstormProjects/untitled/src/logo/economy-svgrepo-com.svg'
-import bulb from '/home/he0982/WebstormProjects/untitled/src/logo/light-bulb-idea-svgrepo-com.svg'
-import business from '/home/he0982/WebstormProjects/untitled/src/logo/presentation-business-and-finance-svgrepo-com.svg'
-import pres from '/home/he0982/WebstormProjects/untitled/src/logo/presentation-svgrepo-com.svg'
-import pyramid from '/home/he0982/WebstormProjects/untitled/src/logo/pyramid-svgrepo-com.svg'
-import sett from '/home/he0982/WebstormProjects/untitled/src/logo/settings-gear-svgrepo-com.svg'
+import economy from './logo/economy.png'
+import bulb from './logo/idea.png'
+import business from './logo/business.png'
+import pres from './logo/planing.png'
+import pyramid from './logo/konkyrent.png'
+import sett from './logo/team.png'
+
 // Тут иконочки
 const TabIcons = [
-    <img src={economy} width="45px" alt="React Logo" />,
+    <img src={economy} width="45px" alt="React Logo" fill="white" />,
     <img src={business} width="45px" alt="React Logo" />,
     <img src={pyramid} width="45px" alt="React Logo" />,
     <img src={pres} width="45px" alt="React Logo" />,
@@ -33,78 +25,79 @@ const App = () => {
     const [userInput, setUserInput] = useState('');
     const [responses, setResponses] = useState(Array(6).fill(''));
     const [currentTab, setCurrentTab] = useState(0);
+    const [ideasHistory, setIdeasHistory] = useState([]);
 // делаем ивенты для юзеримпиута
     const handleInputChange = (event) => {
         setUserInput(event.target.value);
     };
-
-
-
     const handleRequest = async () => {
         try {
-            // делоем запросик по апи или хуй знает как{
-            const response = await axios.post('https://87.242.91.128.sslip.io/api/v1/ml/',{description: userInput});
-            // Assuming the API response contains an array of 6 strings
+            // делоем запросик по апи
+            const response = await axios.post('https://89.232.167.211.sslip.io/api/v1/ml/',{description: userInput});
+            // помещаем наш ответик
             const responsesArray = response.data;
 
-            // Update state with the response data
+            // обновляем состояния
          setResponses(responsesArray);
+         setIdeasHistory((prevHistory) => [...prevHistory, responsesArray[currentTab]]);
         } catch (error) {
             console.error('Error making POST request:', error.message);
         }
     };
     // наши табы задаем
-    const handleTabChange = (_, newTab) => {
-        setCurrentTab(newTab);
+    const handleTabChange = (index) => {
+        setCurrentTab(index);
     };
     const TabNames = ['Экономика', 'Бизнес модель', 'Конкуренты', 'План развития', 'команда', 'ценностное предложение'];
-
+   const createNewIdea = () => {
+       setResponses(Array(6).fill(''));
+       setUserInput(" ")
+       setCurrentTab(0)
+   }
     return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Веб-сервис для проработки идей проектов
-            </Typography>
-            <Paper elevation={3} style={{ marginTop: '20px', padding: '20px' }}>
-                <Typography variant="h5" gutterBottom>
-                    О решения
-                </Typography>
-                <Typography>
-                    Этот веб-сервис позволяет вводить идеи проектов с помощью текстовой области, расположенной ниже. После отправки он генерирует и прорабатывает ключевые параметры проекта основе ваших данных. Старайтесь максимально подробно описывать вашу идею, но не используйте дополнительные описания относительно того, что хотите получить на выходе. Будьте креативны в том, что пишите и не бойтесь перегенерить, если ответ вам не понравился.
-                </Typography>
-            </Paper>
-            <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-                <TextField
-                    label="Напишите вашу идею"
-                    multiline
-                    fullWidth
-                    value={userInput}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    style={{ marginBottom: '20px' }}
-                />
-                <Button variant="contained" color="primary" onClick={handleRequest}>
-                    Отправить
-                </Button>
-            </Paper>
-
-            <Paper elevation={3} style={{ marginTop: '20px', padding: '20px' }}>
-                <Tabs orientation="vertical" value={currentTab} onChange={handleTabChange}>
-                    {TabNames.map((tabName, index) => (
-                        <Tab key={index} label={TabNames[index]} icon={TabIcons[index]} />
-                    ))}
-                </Tabs>
-            </Paper>
-
-            <Paper elevation={10} style={{ marginLeft: '0px', padding: '20px', flex: '1', marginTop: '20px' }}>
-                <div>
-                    <h3>Ответ по разделу: {TabNames[currentTab]}</h3>
-                    {responses[TabNames[currentTab]] && (
-                        <p>{responses[TabNames[currentTab]]}</p>
-                    )}
+    <div className="app">
+        <section className="side-bar">
+            <button onClick={createNewIdea}> New idea</button>
+            <ul className="Tabs" onChange={handleTabChange}>
+                {TabNames.map((tabName, index) => (
+                    <li key={index} onClick={() => handleTabChange(index)} className={currentTab === index ? 'active' : ''}>
+                        {TabIcons[index]}
+                        {tabName}
+                    </li>
+                ))}
+            </ul>
+            <nav>
+                <p> OSS IDEA</p>
+                <div className="ideas-history">
+                    <p>История идей:</p>
+                    <ul>
+                        {ideasHistory.map((idea, index) => (
+                            <li key={index}>{idea}</li>
+                        ))}
+                    </ul>
                 </div>
-            </Paper>
-        </Container>
-    );
+            </nav>
+        </section>
+        <section className="main">
+            <h1> OSS Business-GPT</h1>
+            <ul className="feed">
+                {responses[TabNames[currentTab]] && (
+                    <p>{responses[TabNames[currentTab]]}</p>
+                )}
+            </ul>
+            <div className="bottom-section">
+                <div className="input-container">
+                    <input value={userInput} onChange={handleInputChange}/>
+                    <div id="submit" onClick={handleRequest}> SUBMIT </div>
+                </div>
+                <p className="info">
+                    Base on GigaChat. Free Research Preview.
+                    Our goal is to make AI systems more humanity.
+                    Your feedback will help us improve.
+                </p>
+            </div>
+        </section>
+    </div>);
 };
 
 export default App;
@@ -113,39 +106,3 @@ export default App;
 
 
 
-//   const [data, setData] = useState({});
-//   const [selectedTab, setSelectedTab] = useState(0);
-//
-//   useEffect(() => {
-//     // берем данные с сервера
-//     fetch('')
-//       .then((response) => response.json())
-//       .then((jsonData) => setData(jsonData))
-//       .catch((error) => console.error('Ошибка обработки данных:', error));
-//   }, []);
-//
-//   const handleChange = (event, newValue) => {
-//     setSelectedTab(newValue);
-//   };
-//
-//   return (
-//     <div>
-//       <Tabs value={selectedTab} onChange={handleChange} centered>
-//         {Object.keys(data).map((key, index) => (
-//           <Tab key={index} label={key} />
-//         ))}
-//       </Tabs>
-//       {Object.keys(data).map((key, index) => (
-//         <TabPanel key={index} value={selectedTab} index={index}>
-//           <Typography variant="h5">{data[key]}</Typography>
-//         </TabPanel>
-//       ))}
-//     </div>
-//   );
-// };
-//
-// const TabPanel = ({ children, value, index }) => (
-//   <div role="tabpanel" hidden={value !== index}>
-//     {value === index && <Box p={3}>{children}</Box>}
-//   </div>
-// );
