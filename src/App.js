5439fd0c97b285a353f5a7585078d0a4c4e95ad2
WWSpectr <44667011+MRX1078@users.noreply.github.com>
2023-12-06 15:23:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
     const handleRequest = async () => {
         try {
             // делоем запросик по апи
-            const response = await axios.post('https://87.242.91.128.sslip.io/api/v1/ml/',{description: userInput});
+            const response = await axios.post('https://87.242.91.128.sslip.io/',{description: userInput});
             // помещаем наш ответик
             const responsesArray = response.data;
 
